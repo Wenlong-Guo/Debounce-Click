a2c148aa2d8820216d1909c40c1b2d2c23e86fe1
guowenlong <guowenlong20000@sina.com>
2022-04-12 21:21:12 +0800

    完成V1.0.0

---
+++ Debounce Click/src/main/java/io/github/guowenlong/debounce/ViewExt.kt
@@ -0,0 +1,69 @@
+package io.github.guowenlong.debounce
+
+import android.view.View
+
+/**
+ * Description: View的点击防抖扩展类
+ * Author:      郭文龙
+ * Date:        2022/4/12 18:08
+ * Gmail:       guowenlong20000@sina.com
+ */
+
+/**
+ * 防抖时间间隔的Tag
+ */
+const val TIME_TAG_KEY_TRIGGER_LAST = 1111111111
+
+/**
+ * 最后一次点击生效的时间戳的Tag
+ */
+const val TIME_TAG_KEY_TRIGGER_DELAY = 1111111112
+
+var TRIGGER_DELAY_DEFAULT = 500L
+
+
+/**
+ * 带过滤的点击事件 默认500毫秒
+ */
+fun View.setDebounceClickListener(time: Long = 500, block: (View) -> Unit) {
+    triggerDelay = time
+    setOnClickListener {
+        if (clickEnable()) {
+            block(it)
+        }
+    }
+}
+
+/**
+ * 设置默认防抖时间间隔
+ */
+fun setDebounceDefaultTriggerTimeMillis(timeMillis: Long) {
+    TRIGGER_DELAY_DEFAULT = timeMillis
+}
+
+/**
+ * 自定义属性保存防抖时间间隔
+ */
+private var View.triggerDelay: Long
+    get() = (getTag(TIME_TAG_KEY_TRIGGER_DELAY) as? Long) ?: TRIGGER_DELAY_DEFAULT
+    set(value) {
+        setTag(TIME_TAG_KEY_TRIGGER_DELAY, value)
+    }
+
+/**
+ * 自定义属性保存最后一次点击生效的时间戳
+ */
+private var View.triggerLastTime: Long
+    get() = (getTag(TIME_TAG_KEY_TRIGGER_LAST) as? Long) ?: 0
+    set(value) {
+        setTag(TIME_TAG_KEY_TRIGGER_LAST, value)
+    }
+
+/**
+ * 是否可以点击
+ */
+private fun View.clickEnable(): Boolean {
+    val isEnable = System.currentTimeMillis() - triggerLastTime >= triggerDelay
+    if (isEnable) triggerLastTime = System.currentTimeMillis()
+    return isEnable
+}
---
+++ app/src/main/java/io/github/guowenlong/sample/MainActivity.kt
@@ -0,0 +1,26 @@
+package io.github.guowenlong.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import io.github.guowenlong.debounce.setDebounceClickListener
+import io.github.guowenlong.debounce.setDebounceDefaultTriggerTimeMillis
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var tempTime = System.currentTimeMillis()
+        setDebounceDefaultTriggerTimeMillis(1000)
+        findViewById<TextView>(R.id.tv_content).setDebounceClickListener {
+            Toast.makeText(this, "时间(毫秒)${System.currentTimeMillis()-tempTime}", Toast.LENGTH_SHORT).show()
+            tempTime = System.currentTimeMillis()
+        }
+
+        findViewById<TextView>(R.id.tv_bottom).setDebounceClickListener(2000) {
+            Toast.makeText(this, "时间(毫秒)${System.currentTimeMillis()-tempTime}", Toast.LENGTH_SHORT).show()
+            tempTime = System.currentTimeMillis()
+        }
+    }
+}
